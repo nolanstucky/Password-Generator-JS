5d5263994b244784d084bc023d83d6cd2c557df4
Nolan Stucky <nolan@stucky.net>
2020-12-10 20:55:30 -0800

    updated Readme file and also finished commenting on javascript functions.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Password-Generator-JS
-Password Generator written in JavaScript 
+This password generator was built using html and a custom css styling for the main page. The logic was built in javascript.
+
+# Functionality of this project.
+When user clicks on the generate password button a prompt of character length will be presented allowing the user to create a password between 8-128 characters. They will then be asked if they want lowercase characters, uppercase characters, numbers and or special characters. 
+
+After the user answers all the prompts they will they be presented with a random password that is the character length that they requested filled with the different characters that they requested. The user is also able to click the generate password button creating a new password that replaces the old one. 
--- script.js
+++ script.js
@@ -1,24 +1,33 @@
+//Nolan Stucky
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 //After all prompts randomly generate password with array library.
+//array for lowercase letters
 var aryLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+//array for uppercase letters
 var aryUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//array for numbers
 var aryNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//array for special characters
 var arySpecial = ["!", "@", "#", "$", "%", "^", "&", "*", "`", "~"]
 
 
 
-//When generate password is pressed user is then presented with prompts for password criteria
+//When generate password is called upon user is then presented with prompts for password criteria
 function generatePassword() {
-
+  //password array that will be filled depending on the prompts
   var aryPassword = []
+  //random password variable that will be called upon for the final password
   var randomPassword = "";
   //Prompt that determines length of password from 8 - 128 characters. 
   var passLength = parseInt(prompt("How long do you want your password? Type a number from 8-128."));
+  //if statement that checks if the input is an integer or not.
   if (Number.isInteger(passLength) === false) {
     alert("Password needs to be a numerical value.");
   }
+  //else if statement that makes sure the length is between 8 and 128
   else if (passLength < 8 || passLength > 128) {
     alert("Password needs to be between 8 and 128 characters long.")
   }
@@ -31,7 +40,7 @@ function generatePassword() {
     var passNumbers = confirm("Do you want numbers in your password?");
 
     var passSpecial = confirm("Do you want special characters in your password?");
-
+    //determines if user wants lowercase and adds lowercase array to password array.
     if (passLower === true) {
       aryPassword = aryLower;
     }
@@ -47,38 +56,27 @@ function generatePassword() {
     if (passSpecial === true) {
       aryPassword = aryPassword.concat(arySpecial);
     }
+    //Make sure that edgecase of at least one character type has to be selected
     if (aryPassword.length === 0) {
       alert("Please choose at least one option.");
       return "";
     }
-
+    //After all prompts randomly generate password with array library.
     for (var i = 0; i < passLength; i++) {
       var randomCharacter = aryPassword[Math.floor(Math.random() * aryPassword.length)];
       randomPassword = randomPassword.concat(randomCharacter);
     }
 
   }
-
   //returns the value of the password so it can be written as the text value
   return randomPassword;
 }
-
-
-
-
-
-//Make sure that edgecase of at least one character type has to be selected
-
-
-
-//After all prompts randomly generate password with array library.
-//Replace placeholder text with generated password.
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  //Replace placeholder text with generated password.
   passwordText.value = password;
 
 }
